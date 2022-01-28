a0f1f2be61fb06860cb430f3b25324f853390a05
Lixiang Liu <liulixiang1988@gmail.com>
2022-01-28 10:31:03 +0800

    add word count demo for flink

---
+++ flink-demos/fl-basic/src/main/java/io/lixiang/fl/basic/StringWordCountJob.java
@@ -0,0 +1,41 @@
+package io.lixiang.fl.basic;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class StringWordCountJob {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<String> socketStream = env.socketTextStream("localhost", 9999);
+
+        socketStream.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String s, Collector<String> collector) throws Exception {
+                String[] words = s.split(",");
+                for (String word :
+                        words) {
+                    collector.collect(word);
+                }
+            }
+        }).filter(new FilterFunction<String>() {
+
+            @Override
+            public boolean filter(String s) throws Exception {
+                return StringUtils.isNoneEmpty(s);
+            }
+        }).map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String s) throws Exception {
+                return Tuple2.of(s, 1);
+            }
+        }).keyBy(0).sum(1).print();
+
+    }
+}
