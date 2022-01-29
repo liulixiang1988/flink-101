1a40aeeddd44ff7d6f549759e5e05d2e4144401e
Lixiang Liu <liulixiang1988@gmail.com>
2022-01-29 16:11:10 +0800

    flink ParallelCollection & NumberSequenceIterator

--- flink-demos/fl-datastream/src/main/java/source/SourceApp.java
+++ flink-demos/fl-datastream/src/main/java/source/SourceApp.java
@@ -3,6 +3,7 @@ package source;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.NumberSequenceIterator;
 
 public class SourceApp {
     public static void main(String[] args) throws Exception {
@@ -12,6 +13,13 @@ public class SourceApp {
         // env = StreamExecutionEnvironment.createRemoteEnvironment("localhost", 9999, "user", "password");
         // env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(Configuration.fromMap(null));
 
+        // testParallel(env);
+        testCollectionSource(env);
+
+        env.execute();
+    }
+
+    private static void testParallel(StreamExecutionEnvironment env) {
         // set parallelism for the job
         env.setParallelism(2);
 
@@ -24,6 +32,20 @@ public class SourceApp {
         System.out.println("filter stream: " + filter.getParallelism());
 
         filter.print();
-        env.execute();
+    }
+
+    private static void testCollectionSource(StreamExecutionEnvironment env) {
+        // set parallelism for the job
+        env.setParallelism(2);
+
+        DataStreamSource<Long> stream = env.fromParallelCollection(new NumberSequenceIterator(1, 10), Long.class);
+        System.out.println("socket stream: " + stream.getParallelism());
+
+        SingleOutputStreamOperator<Long> filter = stream.filter(x -> x % 2 == 0);
+        // set parallelism for the operator
+        filter.setParallelism(6);
+        System.out.println("filter stream: " + filter.getParallelism());
+
+        filter.print();
     }
 }
